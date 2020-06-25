c1fe5917b0b84a17e2c916d6a927e875b8ff512d
gsuveer <gargsuveer@gmail.com>
2020-06-24 21:20:11 -0400

    Converted to non-ROS

--- emotion_recognition/src/emotion_recognition.py
+++ emotion_recognition/src/emotion_recognition.py
@@ -9,11 +9,10 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
-import rosbag
-import rospy
-from cv_bridge import CvBridge
-from std_msgs.msg import String, Float32MultiArray
 
+'''
+Function to initialise the model
+'''
 
 def create_model(weights=None):
     # Create the model
@@ -39,13 +38,13 @@ def create_model(weights=None):
     try:
         model.load_weights(weights)
     except:
-        rospy.loginfo("Could not Load Weights")
+        print("Could not Load Weights")
 
     return model
 
-# Takes a RGB frame and the NN model to return the emotion detected on that frame
-
-
+'''
+Takes a RGB frame and the NN model to return the emotion detected on that frame
+'''
 def process_frame(frame, model, facecasc):
 
     # Loads the Haar cascade file for face area detection
@@ -69,77 +68,47 @@ def process_frame(frame, model, facecasc):
     return prediction
 
 
-if __name__ == '__main__':
+'''
+Function that takes in a block of images and return prediction scores on them
+'''
 
-    rospy.init_node('emotion_recognition')
-    rospy.loginfo('started emotion recognition')
+def extract_emotions(images, weights, cascade):
 
-    pre_processing_bag_file = rospy.get_param("~pre_processing_bag_file", None)
-    weights = rospy.get_param("~model_weights", None)
-    topic = rospy.get_param("~topic", None)
-    cascade = rospy.get_param("~cascade", None)
-
-    rospy.loginfo('Bag file: %s', pre_processing_bag_file)
-    rospy.loginfo('Weights file: %s', weights)
-    rospy.loginfo('Topic: %s', topic)
-    rospy.loginfo('Cascade File: %s', cascade)
+    print('Weights file: %s', weights)
+    print('Cascade File: %s', cascade)
 
     #Loading HAARCASCADE File
     if(not(cascade == "")):
         try:
             facecasc = cv2.CascadeClassifier(cascade)
         except:
-            rospy.loginfo("HAARCASCADE object could not be initialised")
+            print("HAARCASCADE object could not be initialised")
     else:
-        rospy.loginfo("No Argument provided for cascade file")
-
-    # Reading Bag File
-    if(not(pre_processing_bag_file == "")):
-        try:
-            pre_processing_bag = rosbag.Bag(pre_processing_bag_file)
-        except:
-            rospy.loginfo("BAG FILE COULD NOT BE READ")
-    else:
-        rospy.loginfo("No Argument provided for Bag Filename")
+        print("No Argument provided for cascade file")
 
     # Creating Model
     if(not(weights == "")):
         try:
             model = create_model(weights)
         except:
-            rospy.loginfo("COULD NOT CREATE MODEL WITH GIVEN WEIGHTS")
+            print("COULD NOT CREATE MODEL WITH GIVEN WEIGHTS")
     else:
-        rospy.loginfo("No Argument provided for model weights")
-
-    if (topic == ""):
-        rospy.loginfo("No Argument provided for topics to read")
-
-    # CvBridge object to convert ROS messages to CV images
-    bridge = CvBridge()
+        print("No Argument provided for model weights")
 
     # Index to emotion Mapping
     emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful",
                     3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-    for topic, frame, t in pre_processing_bag.read_messages(topics=[topic]):
-        # Think about desired encoding
+    n = images.shape[2]
+    prediction_scores_array = np.zeros((7,n))
+    predicted_emotion = []
+    for i in range(n):
         prediction_scores = process_frame(
-            bridge.imgmsg_to_cv2(frame, "rgb8"), model, facecasc)
+            images[:,:,i], model, facecasc)
 
-        print(prediction_scores, t)
-
+        prediction_scores_array[:,i] = prediction_scores
         max_index = int(np.argmax(prediction_scores))
+        predicted_emotion.append(emotion_dict[max_index])
 
-        # Writing information to post_processing_bag
-        scores = Float32MultiArray()
-        scores.data = prediction_scores
 
-        #pre_processing_bag.write(topic + '/prediction_scores', scores)
-
-        predicted_emotion = String()
-        predicted_emotion.data = emotion_dict[max_index]
-
-        #pre_processing_bag.write(
-        #    topic + '/predicted_emotion', predicted_emotion)
-
-    pre_processing_bag.close()
+    return predicted_emotion, prediction_scores_array
