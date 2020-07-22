3a3044a2bc6f962151ff3ba54f81b63e67beead6
gsuveer <gargsuveer@gmail.com>
2020-07-22 17:49:19 -0400

    Removed Bug. Working Now

--- openpose/scripts/extractPoses.py
+++ openpose/scripts/extractPoses.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Wed Jul  8 23:07:00 2020
 
@@ -12,26 +10,23 @@ import os
 import argparse
 import numpy as np
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.append('../python');
-
 try:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    sys.path.append('../python');
     from openpose import pyopenpose as op
 except ImportError as e:
     print('Error: OpenPose library could not be found. The path is probably not set correctly')
     raise e
     
-params = dict()
-params["model_folder"] = "../../models/"
 
 '''
 Function to take a Frame and return keypoints and output image with keypoints
 '''
-def processFrame(Image, opWrapper) :
+def processFrame(img, opWrapper) :
     
     
     datum = op.Datum()
-    datum.cvInputData = np.uint8(Image)
+    datum.cvInputData = np.uint8(img)
     opWrapper.emplaceAndPop([datum])
     cv2.imwrite('output/test.jpg', datum.cvOutputData)
     return datum.cvOutputData, datum.poseKeypoints
@@ -44,8 +39,9 @@ The last channel is taken as number of images
 '''
 
 def processFrames(Images):
-    global params
-    print(params)
+    params = dict()
+    params["model_folder"] = "../../models/"
+    print("Parameters : ", params)
     
     if len(Images.shape)<4 :
         Images = np.expand_dims(Images,-1)
@@ -58,15 +54,18 @@ def processFrames(Images):
     except:
         pass
     
-    OutputPoseKeypoints = np.zeros((num_images,25,3))
-        
+    #OutputPoseKeypoints = np.zeros((num_images,25,3))
+    OutputPoseKeypoints = []
     opWrapper = op.WrapperPython()
     opWrapper.configure(params)
     opWrapper.start()
 
     for i in range(Images.shape[-1]):
-        OutputImage, poseKeypoints = processFrame(Images[:,:,:,i], opWrapper)
+        
+        imageToProcess = Images[:,:,:,i]
+        OutputImage, poseKeypoints = processFrame(imageToProcess, opWrapper)
         OutputImages[:,:,:,i] = OutputImage
-        OutputPoseKeypoints[i,:,:] = poseKeypoints
+        #OutputPoseKeypoints[i,:,:] = poseKeypoints
+        OutputPoseKeypoints.append(poseKeypoints)
     
     return OutputImages, OutputPoseKeypoints
