d4972fdf6d917e2cbf21b25f2e9bc042e8324d30
gsuveer <gargsuveer@gmail.com>
2020-08-10 18:27:59 -0400

    Added Features Path and Range of movement

--- kinematics/extract_profiles.py
+++ kinematics/extract_profiles.py
@@ -11,6 +11,8 @@ import numpy as np
 '''
 Fuction that takes in a 3D keypoints and timestamps
 returns a velocity profile with timestamps 
+
+keypoints shape (n * 25 * 3)
 '''
 def velocity_profile(keypoints, timestamps):
     #Confirm Axis
@@ -38,8 +40,8 @@ Function to take in 3D keypoints and returns range in xyz
 
 def range_of_motion(keypoints):
     
-    max_xyz = np.amax(keypoints, axis=1)
-    min_xyz = np.amin(keypoints, axis=1)
+    max_xyz = np.amax(keypoints, axis=0)
+    min_xyz = np.amin(keypoints, axis=0)
     
     return max_xyz - min_xyz
 
@@ -62,4 +64,32 @@ def jerk(velocity, timestamps):
     jerk = delta2_xyz/delta2_t
     
     return jerk
-     
+
+'''
+Funtion to calculate path(Sum of Distances) from 3D keypoints
+Returns Distance Moved
+keypoints shape (n * 25 * 3)
+'''
+
+def path(keypoints):
+    distance= np.linalg.norm(keypoints, axis=-1)  #should be (n*25)
+    return np.sum(distance, axis=0) #shape 25
+
+'''
+Funtion to calculate range of movement from 3D keypoints and time_stamps
+Returns the difference between max and minimum distance
+keypoints shape (n * 25 * 3)
+'''
+
+def range_of_movement(keypoints):
+    distance= np.linalg.norm(keypoints, axis=-1)  #should be (n*25)
+    max_xyz = np.amax(keypoints, axis=0)
+    min_xyz = np.amin(keypoints, axis=0)
+    return max_xyz - min_xyz #shape 25
+
+'''
+Angular motion
+'''
+
+def angular_motion(keypoints):
+    pass
