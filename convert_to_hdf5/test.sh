6a67342dd64f8318eccf49ac15c230e56e55a16f
Michael Sobrepera <mjsobrep@live.com>
2020-09-11 10:40:12 -0400

    got the system to run cleanly

--- convert_to_hdf5/test.sh
+++ convert_to_hdf5/test.sh
@@ -11,6 +11,5 @@ docker build . --tag hdf5convert
 docker run  \
     --mount type=bind,source=$data,target=/data \
     -it \
-    hdf5convert \
-    bash
-    #-c "roslaunch convert_to_hdf5 main.launch"
+    --rm \
+    hdf5convert
