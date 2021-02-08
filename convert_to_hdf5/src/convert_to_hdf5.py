d31ffc403571b56cc573e721ddba12070f1a5607
Michael Sobrepera <mjsobrep@live.com>
2021-02-08 02:32:47 -0500

    got hdf5 building on aws

--- convert_to_hdf5/src/convert_to_hdf5.py
+++ convert_to_hdf5/src/convert_to_hdf5.py
@@ -100,7 +100,11 @@ def get_topic_info(data_info_mapping, bag_file, meta_data):
         rospy.loginfo(
             'Extracting meta info from standard video topic: %s', vid_topic)
         rospy.loginfo('\t\tmapped to real info topic: %s', true_topic)
-        _, msg, _ = next(bag_file.read_messages([true_topic]))
+        try:
+            _, msg, _ = next(bag_file.read_messages([true_topic]))
+        except StopIteration:
+            topic_info[vid_topic] = None
+            continue
         topic_info[vid_topic] = msg
     return topic_info
 
@@ -108,11 +112,12 @@ def get_topic_info(data_info_mapping, bag_file, meta_data):
 def load_hdf_files(record_names, out_dir, data_info_mapping, meta_data, topic_info):
     hdf5_files = len(record_names)*[None]
     for idx, hdf5_fn in enumerate(record_names):
+        rospy.loginfo('Working on seq/act: %s', hdf5_fn)
         hdf5_fn_full = pathlib.Path(out_dir, hdf5_fn).with_suffix('.hdf5')
         pth = pathlib.Path(hdf5_fn_full)
         pth.parent.mkdir(parents=True, exist_ok=True)
         try:
-            hdf5_files[idx] = h5py.File(hdf5_fn_full, 'w')
+            hdf5_files[idx] = h5py.File(hdf5_fn_full, 'a')
         except:  # pylint: disable=bare-except
             rospy.logerr(
                 'HDF5 Database COULD NOT BE READ/CREATED: %s', hdf5_fn_full)
@@ -121,61 +126,65 @@ def load_hdf_files(record_names, out_dir, data_info_mapping, meta_data, topic_in
         for vid_topic in data_info_mapping:
             raw_topic = meta_data['bag-mapping'][vid_topic]
             rospy.loginfo(
-                'Creating new Datasets for standard topic : %s' % (vid_topic))
+                '\tParsing for standard topic : %s' % (vid_topic))
             rospy.loginfo(
                 '\t\traw topic : %s' % (raw_topic))
             topic_meta_info = topic_info[vid_topic]
+            if topic_meta_info:
+                hdf5_database = hdf5_files[idx]
+                vid_topic_data_name = vid_topic + '/data'
+                vid_topic_secs_name = vid_topic + '/secs'
+                vid_topic_nsecs_name = vid_topic + '/nsecs'
+                if 'depth' in vid_topic and vid_topic_data_name not in hdf5_database:
+                    rospy.loginfo('\t\tnew depth dataset')
+                    dataset = hdf5_database.create_dataset(vid_topic_data_name,
+                                                           (INITIAL_SIZE, topic_meta_info.height,
+                                                            topic_meta_info.width),
+                                                           maxshape=(
+                                                               None, topic_meta_info.height,
+                                                               topic_meta_info.width),
+                                                           dtype=np.uint16,
+                                                           chunks=(
+                                                               CHUNK_SIZE,
+                                                               topic_meta_info.height,
+                                                               topic_meta_info.width))
+                elif 'color' in vid_topic and vid_topic_data_name not in hdf5_database:
+                    rospy.loginfo('\t\tnew color dataset')
+                    dataset = hdf5_database.create_dataset(vid_topic_data_name,
+                                                           (INITIAL_SIZE, topic_meta_info.height,
+                                                            topic_meta_info.width, 3),
+                                                           maxshape=(
+                                                               None, topic_meta_info.height,
+                                                               topic_meta_info.width, 3),
+                                                           dtype=np.uint8,
+                                                           chunks=(
+                                                               CHUNK_SIZE,
+                                                               topic_meta_info.height,
+                                                               topic_meta_info.width,
+                                                               3))
+                else:
+                    rospy.loginfo(
+                        '\t\tDataset already existed!! no action taken')
+                    continue
 
-            hdf5_database = hdf5_files[idx]
-            vid_topic_data_name = vid_topic + '/data'
-            vid_topic_secs_name = vid_topic + '/secs'
-            vid_topic_nsecs_name = vid_topic + '/nsecs'
-            if 'depth' in vid_topic and vid_topic_data_name not in hdf5_database:
-                dataset = hdf5_database.create_dataset(vid_topic_data_name,
-                                                       (INITIAL_SIZE, topic_meta_info.height,
-                                                        topic_meta_info.width),
-                                                       maxshape=(
-                                                           None, topic_meta_info.height,
-                                                           topic_meta_info.width),
-                                                       dtype=np.uint16,
-                                                       chunks=(
-                                                           CHUNK_SIZE,
-                                                           topic_meta_info.height,
-                                                           topic_meta_info.width),
-                                                       compression='szip')
-            elif 'color' in vid_topic and vid_topic_data_name not in hdf5_database:
-                dataset = hdf5_database.create_dataset(vid_topic_data_name,
-                                                       (INITIAL_SIZE, topic_meta_info.height,
-                                                        topic_meta_info.width, 3),
-                                                       maxshape=(
-                                                           None, topic_meta_info.height,
-                                                           topic_meta_info.width, 3),
-                                                       dtype=np.uint8,
-                                                       chunks=(
-                                                           CHUNK_SIZE,
-                                                           topic_meta_info.height,
-                                                           topic_meta_info.width,
-                                                           3),
-                                                       compression='szip')
+                hdf5_database.create_dataset(vid_topic_secs_name, (INITIAL_SIZE,),
+                                             maxshape=(None,),
+                                             dtype=np.float32, chunks=(CHUNK_SIZE,))
+
+                hdf5_database.create_dataset(vid_topic_nsecs_name, (INITIAL_SIZE,),
+                                             maxshape=(None,),
+                                             dtype=np.float32, chunks=(CHUNK_SIZE,))
+
+                dataset.attrs.create('height', topic_meta_info.height)
+                dataset.attrs.create('width', topic_meta_info.width)
+                dataset.attrs.create('D', topic_meta_info.D)
+                dataset.attrs.create('K', topic_meta_info.K)
+                dataset.attrs.create('R', topic_meta_info.R)
+                dataset.attrs.create('P', topic_meta_info.P)
+                dataset.attrs.create('binning_x', topic_meta_info.binning_x)
+                dataset.attrs.create('binning_y', topic_meta_info.binning_y)
             else:
-                continue
-
-            hdf5_database.create_dataset(vid_topic_secs_name, (INITIAL_SIZE,),
-                                         maxshape=(None,),
-                                         dtype=np.float32, chunks=True)
-
-            hdf5_database.create_dataset(vid_topic_nsecs_name, (INITIAL_SIZE,),
-                                         maxshape=(None,),
-                                         dtype=np.float32, chunks=True)
-
-            dataset.attrs.create('height', topic_meta_info.height)
-            dataset.attrs.create('width', topic_meta_info.width)
-            dataset.attrs.create('D', topic_meta_info.D)
-            dataset.attrs.create('K', topic_meta_info.K)
-            dataset.attrs.create('R', topic_meta_info.R)
-            dataset.attrs.create('P', topic_meta_info.P)
-            dataset.attrs.create('binning_x', topic_meta_info.binning_x)
-            dataset.attrs.create('binning_y', topic_meta_info.binning_y)
+                rospy.loginfo('\t\tNo meta info')
     return hdf5_files
 
 
@@ -187,7 +196,7 @@ def node():
     meta_filename = rospy.get_param('~meta_file', None)
     rospy.loginfo('meta Filename: %s', meta_filename)
     out_dir = rospy.get_param('~out_dir', None)
-    rospy.loginfo('output director: %s', out_dir)
+    rospy.loginfo('output directory: %s', out_dir)
 
     bag_file = load_bag_file(bag_filename)
     meta_data = load_meta_file(meta_filename)
@@ -199,25 +208,28 @@ def node():
         record_names, out_dir, data_info_mapping, meta_data, topic_info)
 
     bridge = CvBridge()
-    for vid_topic in tqdm(data_info_mapping):
-        rospy.loginfo('Extracting info from : %s' % (vid_topic))
+    outer_progress_bar = tqdm(data_info_mapping)
+    for vid_topic in outer_progress_bar:
+        # rospy.loginfo('Extracting info from : %s' % (vid_topic))
         raw_topic = meta_data['bag-mapping'][vid_topic]
-        rospy.loginfo('\t\tWith raw topic name : %s' % (raw_topic))
+        # rospy.loginfo('\t\tWith raw topic name : %s' % (raw_topic))
         vid_topic_data_name = vid_topic + '/data'
         vid_topic_secs_name = vid_topic + '/secs'
         vid_topic_nsecs_name = vid_topic + '/nsecs'
-        resizing_time = 0
-        writing_time = 0
-        bridge_time = 0
-        first_time = 0
-        second_time = 0
-        bag_read_time = 0
-        clk_total = time.perf_counter()
+        # resizing_time = 0
+        # writing_time = 0
+        # bridge_time = 0
+        # first_time = 0
+        # second_time = 0
+        # bag_read_time = 0
+        # clk_total = time.perf_counter()
+        outer_progress_bar.set_description('Working on: {}'.format(vid_topic))
+
         with tqdm(total=bag_file.get_message_count(raw_topic)) as inner_progress_bar:
-            clck = time.perf_counter()
+            # clck = time.perf_counter()
             for _, msg, _ in bag_file.read_messages([raw_topic]):
-                bag_read_time += time.perf_counter()-clck
-                clck = time.perf_counter()
+                # bag_read_time += time.perf_counter()-clck
+                # clck = time.perf_counter()
                 timestamp = msg.header.stamp.secs + 1e-9*msg.header.stamp.nsecs
                 gts = timestamp > np.asarray(start_times)
                 lte = timestamp < np.asarray(end_times)
@@ -237,48 +249,48 @@ def node():
                 dataset = hdf5_file[vid_topic_data_name]
                 timestamp_secs = hdf5_file[vid_topic_secs_name]
                 timestamp_nsecs = hdf5_file[vid_topic_nsecs_name]
-                first_time += time.perf_counter()-clck
+                # first_time += time.perf_counter()-clck
 
-                clck = time.perf_counter()
+                # clck = time.perf_counter()
                 dataset.resize(dataset.shape[0]+1, axis=0)
                 timestamp_secs.resize(timestamp_secs.shape[0]+1, axis=0)
                 timestamp_nsecs.resize(timestamp_nsecs.shape[0]+1, axis=0)
-                resizing_time += time.perf_counter()-clck
+                # resizing_time += time.perf_counter()-clck
 
                 if 'depth' in vid_topic:
-                    clck = time.perf_counter()
+                    # clck = time.perf_counter()
                     img = bridge.imgmsg_to_cv2(msg, '16UC1')
-                    bridge_time += time.perf_counter()-clck
-                    clck = time.perf_counter()
+                    # bridge_time += time.perf_counter()-clck
+                    # clck = time.perf_counter()
                     dataset[dataset.shape[0]-1, :,
                             :] = img
-                    writing_time += time.perf_counter()-clck
+                    # writing_time += time.perf_counter()-clck
                 else:
-                    clck = time.perf_counter()
+                    # clck = time.perf_counter()
                     img = bridge.imgmsg_to_cv2(msg, 'bgr8')
-                    bridge_time += time.perf_counter()-clck
-                    clck = time.perf_counter()
+                    # bridge_time += time.perf_counter()-clck
+                    # clck = time.perf_counter()
                     dataset[dataset.shape[0]-1, :, :,
                             :] = img
-                    writing_time += time.perf_counter()-clck
+                    # writing_time += time.perf_counter()-clck
 
-                clck = time.perf_counter()
+                # clck = time.perf_counter()
                 timestamp_secs[timestamp_secs.shape[0] -
                                1] = msg.header.stamp.secs
                 timestamp_nsecs[timestamp_nsecs.shape[0] -
                                 1] = msg.header.stamp.nsecs
-                writing_time += time.perf_counter()-clck
-                clck = time.perf_counter()
+                # writing_time += time.perf_counter()-clck
+                # clck = time.perf_counter()
                 inner_progress_bar.update(1)
-                second_time = time.perf_counter()-clck
-                clck = time.perf_counter()
-            total_time = time.perf_counter()-clk_total
-            rospy.loginfo('writing time:  {}'.format(writing_time/total_time))
-            rospy.loginfo('bridge time:   {}'.format(bridge_time/total_time))
-            rospy.loginfo('resizing time: {}'.format(resizing_time/total_time))
-            rospy.loginfo('first time: {}'.format(first_time/total_time))
-            rospy.loginfo('second time: {}'.format(second_time/total_time))
-            rospy.loginfo('bag read time: {}'.format(bag_read_time/total_time))
+                # second_time = time.perf_counter()-clck
+                # clck = time.perf_counter()
+            # total_time = time.perf_counter()-clk_total
+            # rospy.loginfo('writing time:  {}'.format(writing_time/total_time))
+            # rospy.loginfo('bridge time:   {}'.format(bridge_time/total_time))
+            # rospy.loginfo('resizing time: {}'.format(resizing_time/total_time))
+            # rospy.loginfo('first time: {}'.format(first_time/total_time))
+            # rospy.loginfo('second time: {}'.format(second_time/total_time))
+            # rospy.loginfo('bag read time: {}'.format(bag_read_time/total_time))
     for file in hdf5_files:
         file.close()
     rospy.loginfo('Exiting Process')
--- convert_to_hdf5/src/process-in-ec2.sh
+++ convert_to_hdf5/src/process-in-ec2.sh
@@ -23,27 +23,36 @@ subject_padded=$(printf '%03d' "$subject")
 echo "Processing for subj: $subject_padded"
 
 src=s3://flo-exp-aim1-data-raw/$subject_padded/
-out=s3://flo-exp-aim1-hdf5/$subject_padded/
-meta=s3://flo-exp-aim1-data-meta/$subject_padded/
+meta=s3://flo-exp-aim1-data-meta/$subject_padded/meta.yaml
 #src=/media/mjsobrep/flo-external/008/ # <- this should get pulled in from batch job
 # copy from s3 into local storage
 
 # note: subfolders: ros, 3rd-person, gopro
 
+#Get the ros files of interest, note that there are some parameter files that we need to ignore, hence the grep
 bag_files=$(aws s3 ls "$src"'ros/'| awk '{print $4}' | grep bag.bz2)
 
-aws s3 cp "$src"'meta.yaml' tmp/
+mkdir /data/ros/
 
-prior_pid=-1
+aws s3 cp "$meta" /data/ros/
+
 for bag_fn in $bag_files
 do
-    aws s3 cp "$src"'ros/'"$bag_fn" tmp/ && lbzip2 -d "/tmp/$bag_fn"
-    if ((prior_pid > 0)); then
-        wait $prior_pid
-    fi
-    # have to watch out, the bag filename has the .bgz file ending
-    roslaunch convert_to_hdf5 main.launch target:="/tmp/${$bag_fn:0:-4}" out_dir:="/tmp" meta_file:="tmp/meta.yaml" && rm ${$bag_fn:0:-4} &
-    prior_pid=$!
+    #bring in the bag file from s3
+    aws s3 cp "$src"'ros/'"$bag_fn" /data/ros/
+
+    lbzip2 -d "/data/ros/$bag_fn"
+
+    docker run  \
+        --mount type=bind,source="/data/ros",target=/data \
+        -it \
+        --rm \
+        hdf5convert \
+        roslaunch convert_to_hdf5 convert_to_hdf5.launch bag_file:="/data/${bag_fn%.*}" out_dir:="/data" meta_file:="/data/meta.yaml"
+
+    rm "/data/ros/${bag_fn%.*}"
 done
 
-aws s3 cp tmp/*.hdf5 "$out"
+echo "--------------------------------------"
+echo "--- Done generating HDF5 files !!!!---"
+echo "--------------------------------------"
