1e6980795921f711899a16e4a9d5429186f3ac23
gsuveer <gargsuveer@gmail.com>
2021-03-18 01:24:59 -0400

    Tested Depth matching

---
+++ convert_to_hdf5/test_depth.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+
+
+
+data="/media/gsuveer/391cd01c-d5a2-4313-947a-da8978447a80/gsuveer/Desktop/Flo_data"
+bag="/data/flo_recording_2020-12-15-11-39-59_1.bag" 
+meta="/data/meta.yaml"
+code="/home/gsuveer/catkin_ws/src/LilFloAssessmentPipeline/convert_to_hdf5"
+
+
+echo "Saving output to: ${data}"
+echo "Working with bag file: ${bag}"
+
+
+if [ "$rebuild" = true ] ; then
+    echo 'rebuilding docker image'
+    docker build . --tag hdf5convert
+fi
+
+docker run  \
+    --mount type=bind,source="$data",target=/data \
+    --mount type=bind,source="$code",target=/catkin_ws/src/convert_to_hdf5 \
+    -it \
+    --rm \
+    hdf5convert \
+    bash
+#roslaunch convert_to_hdf5 convert_to_hdf5.launch bag_file:="$bag" out_dir:="/data" meta_file:="$meta"
