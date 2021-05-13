f78348e3b1e426a21479570b25aaa9986467f5dc
gsuveer <gargsuveer@gmail.com>
2021-05-13 11:14:18 -0400

    Rebasing for cleanliness

---
+++ visualize/video_overlay/run-locally.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+
+# parse options
+while getopts :s:c:a:p:d:o: flag
+do
+    case "${flag}" in
+        s) subject=${OPTARG};;
+        c) condition=${OPTARG};;
+        a) activity=${OPTARG};;
+        p) camera=${OPTARG};;
+        o) overlay=${OPTARG};;
+	d) data=${OPTARG};;
+        :) echo 'missing argument' >&2; exit 1;;
+        \?) echo 'invalid option' >&2; exit 1
+    esac
+done
+
+# check subject number
+echo "Passed: $subject"
+re='^[0-9]+$'
+if ! [[ $subject =~ $re ]] ; then
+   echo "error: Subject number is not a number" >&2; exit 1
+fi
+
+if [[ "$camera" =~ ^(upper|lower|all)$ ]]; then
+    echo "processing for $camera realsense"
+else
+    echo "invalid camera parameter passed: $camera"
+fi
+
+subject_padded=$(printf '%03d' "$subject")
+echo "Processing for subj: $subject_padded"
+echo "Data directory: $data"
+echo "Condition: $condition"
+echo "Activity: $activity"
+echo "Camera: $camera"
+
+if [[ "$camera" == "all" ]]; then
+
+sudo docker run \
+    --mount type=bind,source="$data",target=/code/data \
+    --rm \
+    video-overlay \
+    "data/$condition/$activity" \
+    "lower"\
+    "$overlay"
+
+sudo docker run \
+    --mount type=bind,source="$data",target=/code/data \
+    --rm \
+    video-overlay \
+    "data/$condition/$activity"\
+    "upper"\
+    "$overlay"
+else
+sudo docker run \
+    --mount type=bind,source="$data",target=/code/data \
+    --rm \
+    video-overlay\
+    "data/$condition/$activity"\
+    "$camera"\
+    "$overlay"
+fi
--- visualize/video_overlay/skeleton_overlay2d.py
+++ visualize/video_overlay/skeleton_overlay2d.py
@@ -9,13 +9,14 @@ import numpy as np
 from utils import draw_text
 from utils import colorScale
 
-def overlay_wrists(file_stub, cam):
+def overlay_2dSkeleton(file_stub, cam):
     hdf5_video = h5py.File(file_stub+'.hdf5')
     hdf5_tracking = h5py.File(file_stub+'-novid.hdf5')
 
     dset = 'vid/color/data/{}/data'.format(cam)
     video_writer = cv2.VideoWriter(
-        file_stub+'-'+cam+'-wrist.avi', cv2.VideoWriter_fourcc(*'MJPG'), 30, (1920, 1080))
+        file_stub+'-'+cam+'-2dskel.avi', cv2.VideoWriter_fourcc(*'MJPG'), 30, (1920, 1080))
+
     for idx in trange(hdf5_video[dset].shape[0]):
         img = hdf5_video[dset][idx]
         keypoints = hdf5_tracking[dset+'-keypoints'][idx]
@@ -30,13 +31,25 @@ def overlay_wrists(file_stub, cam):
 
         # Joints listed here: https://github.com/CMU-Perceptual-Computing-Lab/openpose/
         # blob/master/doc/02_output.md#keypoints-in-cpython
-        for joint in (4, 7):
+        for joint in range(0, 9):
             x = int(keypoints[joint][0])
             y = int(keypoints[joint][1])
-            cv2.circle(img, (x, y), 20,
+            cv2.circle(img, (x, y), 10,
                        colorScale(confidence[4], 0, 1), 8)
 
+        pairs = [(0,1), (4,3), (3,2), (2,1) ,(1,5), (5,6), (6,7), (1,8)]
+        for joint in pairs:
+            xs = int(keypoints[joint[0]][0])
+            ys = int(keypoints[joint[0]][1])
+
+            xe = int(keypoints[joint[1]][0])
+            ye = int(keypoints[joint[1]][1])
+
+            if confidence[joint[0]] > 0.5 and confidence[joint[1]] > 0.5 :
+                cv2.line(img, (xs, ys), (xe, ye), colorScale(confidence[joint[0]] + confidence[joint[1]] , 0, 1))
+
         video_writer.write(img)
+
     video_writer.release()
     hdf5_video.close()
     hdf5_tracking.close()
