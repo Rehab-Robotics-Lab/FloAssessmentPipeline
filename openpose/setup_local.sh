c70c0cc8fb6af40f49f8963a3eb6a36fb8ba50b6
gsuveer <gargsuveer@gmail.com>
2021-05-12 20:02:45 -0400

    Double quotes for bash linting

--- openpose/setup_local.sh
+++ openpose/setup_local.sh
@@ -19,8 +19,8 @@ sudo groupadd docker
 sudo usermod -aG docker "$USER"
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+   && curl -s -L "https://nvidia.github.io/nvidia-docker/gpgkey" | sudo apt-key add - \
+   && curl -s -L "https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list" | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
 sudo apt-get install -y nvidia-docker2
 sudo apt-get clean
