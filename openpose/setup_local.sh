529ead60d894aeaa578eb75d6dae09d217c5b739
gsuveer <gargsuveer@gmail.com>
2021-05-12 20:21:57 -0400

    I will pass linting

--- openpose/setup_local.sh
+++ openpose/setup_local.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -o errexit
 set -o pipefail
 
@@ -11,14 +12,16 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 sudo apt-get update
 sudo apt-get -y install cuda
-echo 'export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}' >> "$HOME/.bashrc"
+# shellcheck disable=SC2016
+echo "export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}" >> "$HOME/.bashrc"
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo groupadd docker
 sudo usermod -aG docker "$USER"
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+# shellcheck disable=SC1091
+distribution=$(. /etc/os-release;echo "$ID""$VERSION_ID") \
    && curl -s -L "https://nvidia.github.io/nvidia-docker/gpgkey" | sudo apt-key add - \
    && curl -s -L "https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list"
 sudo apt-get update
