4e08e6a89ba43f766fabb5e3fb2ec7c2145de810
gsuveer <gargsuveer@gmail.com>
2021-05-20 11:55:28 -0400

    hdf5 files module

--- kinematics/scripts/process_hdf5.py
+++ kinematics/scripts/process_hdf5.py
@@ -25,26 +25,47 @@ def extract_kinematics(pth):
     print('opened hdf file')
 
     topic_3dkeypoints = 'vid/color/data/lower/data-3dkeypoints-stereo'
+    timestamps = np.asarray(hdf5_tracking['vid/color/data/lower/secs']) * 1e-9 + \
+                 np.asarray(hdf5_tracking['vid/color/data/lower/nsecs'])
+
     keypoints  = hdf5_tracking[topic_3dkeypoints]
     print(keypoints.shape)
 
-    right_wrist_kp = extract_right_wrist_points(keypoints)
-    left_wrist_kp = extract_left_wrist_points(keypoints)
+    right_wrist_kp = ep.extract_right_wrist_points(keypoints)
+    left_wrist_kp = ep.extract_left_wrist_points(keypoints)
 
-    rw_vel = ep.velocity_profile(right_wrist_kp, timestamps)
-    lw_vel = ep.velocity_profile(left_wrist_kp, timestamps)
+    print(right_wrist_kp.shape)
+    print(left_wrist_kp.shape)
+
+    rw_vel, timestamps_vel = ep.velocity_profile(right_wrist_kp, timestamps)
+    lw_vel, timestamps_vel = ep.velocity_profile(left_wrist_kp, timestamps)
+
+    print("rw_vel :", rw_vel.shape)
+    print("lw_vel :", lw_vel.shape)
 
     rw_range_mov = ep.range_of_movement(right_wrist_kp)
     lw_range_mov = ep.range_of_movement(left_wrist_kp)
 
-    rw_jerk = ep.jerk(rw_vel, timestamps)
-    lw_jerk = ep.jerk(lw_vel, timestamps)
+    print(rw_range_mov.shape)
+    print(lw_range_mov.shape)
 
-    rw_time_to_max = ep.time_to_max_velocity(rw_vel, timestamps)
-    lw_time_to_max = ep.time_to_max_velocity(lw_vel, timestamps)
+    rw_time_to_max = ep.time_to_max_velocity(rw_vel, timestamps_vel)
+    lw_time_to_max = ep.time_to_max_velocity(lw_vel, timestamps_vel)
 
-    rw_range_motion = ep.range_of_motion(rw_vel, timestamps)
-    lw_range_motion = ep.range_of_motion(lw_vel, timestamps)
+    print(rw_time_to_max.shape)
+    print(lw_time_to_max.shape)
+
+    rw_range_motion = ep.range_of_motion(rw_vel, timestamps_vel)
+    lw_range_motion = ep.range_of_motion(lw_vel, timestamps_vel)
+
+    print(rw_range_motion.shape)
+    print(lw_range_motion.shape)
+
+    rw_jerk = ep.jerk(rw_vel, timestamps_vel)
+    lw_jerk = ep.jerk(lw_vel, timestamps_vel)
+
+    print(rw_jerk.shape)
+    print(lw_jerk.shape)
 
     #Make Feature Vector here (8 Features)
 
