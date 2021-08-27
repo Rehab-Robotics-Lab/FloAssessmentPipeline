6a576373ec55d21b6f4ea3643f98ce447c3c504c
suveergarg <gargsuveer@gmail.com>
2021-08-27 12:20:21 -0400

    Visualization changes - apply this to vis branch

--- visualize/video_overlay/skeleton_3d.py
+++ visualize/video_overlay/skeleton_3d.py
@@ -12,13 +12,12 @@ import math
 import sys
 from utils import draw_text
 from utils import colorScale
-import kinematics.extract_profiles as k
+import kinematics.scripts.extract_profiles as k
 import copy
 
 # Inspiration from: https://medium.com/@pnpsegonne/animating-a-3d-scatterplot-with-matplotlib-ca4b676d4b55
 
-
-def animate(iteration, data, scatters, lines, texts, ax, joint_pairs, keypoints):
+def animate(iteration, data, scatters, lines, texts, ax, joint_pairs, keypoints, frames):
     for i in keypoints:
         scatters[i]._offsets3d = (data[iteration, i, 0:1], data[iteration, i, 1:2], data[iteration, i, 2:])
 
@@ -31,7 +30,34 @@ def animate(iteration, data, scatters, lines, texts, ax, joint_pairs, keypoints)
         x, y, _ = proj3d.proj_transform(data[iteration, i, 0], data[iteration, i, 1], data[iteration, i, 2], ax.get_proj())
         text.set_position((x, y))
 
-    return scatters, lines, texts
+    axis_color = ['r', 'g', 'b']
+    all_quivers = []
+    for frame, quivers in frames.items():
+        if frame == "right_shoulder_fixed":
+            origin = data[iteration, 2, :]    
+            dirs = k.right_shoulder_fixed_frame(data[iteration, :, :])
+        elif frame == "right_shoulder_moving":
+            origin = data[iteration, 2, :]
+            dirs = k.right_shoulder_moving_frame(data[iteration, :, :])
+        elif frame == "left_shoulder_fixed":
+            origin = data[iteration, 5, :]    
+            dirs = k.left_shoulder_fixed_frame(data[iteration, :, :])
+        elif frame == "left_shoulder_moving":
+            origin = data[iteration, 5, :]
+            dirs = k.left_shoulder_moving_frame(data[iteration, :, :])
+        
+        new_quivers = []
+        for i, quiver in enumerate(quivers):
+            quiver.remove()
+            quiver = ax.quiver(origin[0], origin[1], origin[2], dirs[0,i], dirs[1,i], dirs[2,i], 
+                                length = 0.1, normalize = True, color = axis_color[i])
+            
+            new_quivers.append(quiver)
+            all_quivers.append(quiver)
+        
+        frames[frame] = new_quivers
+        
+    return scatters, lines, texts, frames, all_quivers
 
 
 def skeleton_3d(file_stub, cam, save = False, show = False):
@@ -60,11 +86,30 @@ def skeleton_3d(file_stub, cam, save = False, show = False):
              size=5,
              zorder=1, color='k') for i in range(len(annotations))]
 
-
+    #Making frames using quivers to attack to shoulders
     frames = {}
+    axis_color = ['r', 'g', 'b']
 
-    r_fixed_frame = k.right_shoulder_fixed_frame(points3d[0])
-    rs_f = [ax.quiver(points3d[0, , 2],) for i in range(3)]
+    rsf = k.right_shoulder_fixed_frame(points3d[0])
+    rsf_quivers = [ax.quiver(points3d[0, 2, 0], points3d[0, 2, 1], points3d[0, 2, 2], 
+                    rsf[0,i], rsf[1,i], rsf[2,i], length = 0.1, normalize = True, color = axis_color[i]) for i in range(3)]
+    
+    rs = k.right_shoulder_moving_frame(points3d[0])
+    rs_quivers = [ax.quiver(points3d[0, 2, 0], points3d[0, 2, 1], points3d[0, 2, 2], 
+                    rs[0,i], rs[1,i], rs[2,i], length = 0.1, normalize = True, color = axis_color[i]) for i in range(3)]
+
+    lsf = k.left_shoulder_fixed_frame(points3d[0])
+    lsf_quivers = [ax.quiver(points3d[0, 5, 0], points3d[0, 5, 1], points3d[0, 5, 2], 
+                    lsf[0,i], lsf[1,i], lsf[2,i], length = 0.1, normalize = True, color = axis_color[i]) for i in range(3)]
+    
+    ls = k.left_shoulder_moving_frame(points3d[0])
+    ls_quivers = [ax.quiver(points3d[0, 5, 0], points3d[0, 5, 1], points3d[0, 5, 2], 
+                    ls[0,i], ls[1,i], ls[2,i], length = 0.1, normalize = True, color = axis_color[i]) for i in range(3)]
+    
+    frames["right_shoulder_fixed"] = rsf_quivers
+    frames["right_shoulder_moving"] = rs_quivers
+    frames["left_shoulder_fixed"] = lsf_quivers
+    frames["left_shoulder_moving"] = ls_quivers
 
     ax.set_xlim3d([-1, 1])
     ax.set_xlabel('X')
@@ -81,7 +126,7 @@ def skeleton_3d(file_stub, cam, save = False, show = False):
     ani = animation.FuncAnimation(fig,
                                   animate,
                                   len(points3d),
-                                  fargs=(points3d, scatters, lines, texts, ax, joint_pairs, keypoints),
+                                  fargs=(points3d, scatters, lines, texts, ax, joint_pairs, keypoints, frames),
                                   interval=50)
 
     if save:
