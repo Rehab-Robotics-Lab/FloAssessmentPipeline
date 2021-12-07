39a8da9a1c4e4a3ff24f72a81c8e687120f31adc
Michael Sobrepera <mjsobrep@live.com>
2021-12-07 07:35:32 -0800

    finished and tested data upload script

--- upload_data/upload_to_oci.sh
+++ upload_data/upload_to_oci.sh
@@ -4,43 +4,83 @@ set -o pipefail
 
 BUCKET_NAME='sandbox'
 
-while getopts :s: flag
+while getopts :p:t: flag
 do
     case "${flag}" in
-        s) src_dir=${OPTARG};;
+        t) src_dir=${OPTARG};;
+        p) prefix=${OPTARG};;
         :) echo 'missing argument' >&2; exit 1;;
         \?) echo 'invalid option' >&2; exit 1
     esac
 done
 
+prefix="${prefix%/}/"
+
 refresh_fun(){
+    echo "token refresh background job started (PID=$BASHPID)"
     while :
     do
+        echo 'background job refreshing token'
         sleep $((  60*30  )) # 60 sec * 30 minutes
-        oci session refresh --profile 'rrl-oci-profile'
+        oci session refresh --profile 'rrl-oci-profile-data-upload'
     done
-
+    echo 'exiting background job'
 }
 
-oci session authenticate --region us-ashburn-1 --tenancy-name 'upennrehabrobotics' --profile-name 'rrl-oci-profile'
+oci session authenticate \
+    --region us-ashburn-1 \
+    --tenancy-name 'upennrehabrobotics' \
+    --profile-name 'rrl-oci-profile-data-upload'
+
 refresh_fun &
 background_pid=$!
+echo "launched background token maintenance (PID=$background_pid)"
 
-oci os object sync --bucket-name $BUCKET_NAME --dry-run --src-dir "$src_dir"
+cleanup(){
+    echo ''
+    printf "Cleaning up session\n"
+    kill "$background_pid" || true
+    oci session terminate \
+        --config-file "$HOME/.oci/config" \
+        --profile 'rrl-oci-profile-data-upload' \
+        --auth security_token
+    echo 'done cleaning up'
+}
+
+trap cleanup EXIT
+
+oci os object sync \
+    --bucket-name $BUCKET_NAME \
+    --dry-run \
+    --src-dir "$src_dir" \
+    --prefix "$prefix" \
+    --config-file "$HOME/.oci/config" \
+    --profile 'rrl-oci-profile-data-upload' \
+    --auth security_token
+echo "will upload with prefix: $prefix"
 
 while :
 do
     read  -r -n 1 -p "would you like to proceed with upload? (y/n)" proceed
+    echo ''
 
     if [ "$proceed" = "n" ]; then
-       kill $background_pid
+        echo 'you said no'
        exit 0
     elif [ "$proceed" = "y" ];then
+        echo 'you said yes'
         break
     fi
+    echo 'you need to say yes: y or no: n!!'
 done
 
-oci os object sync --bucket-name $BUCKET_NAME --src-dir "$src_dir"
+echo 'beginning upload'
+oci os object sync \
+    --bucket-name $BUCKET_NAME \
+    --src-dir "$src_dir" \
+    --prefix "$prefix" \
+    --config-file "$HOME/.oci/config" \
+    --profile 'rrl-oci-profile-data-upload' \
+    --auth security_token
 
-kill $background_pid
-echo 'done cleaning up'
+echo 'upload complete'
