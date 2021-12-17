901f7d0edf1085026cf639485426acdf3c26735c
Michael Sobrepera <mjsobrep@live.com>
2021-12-17 13:34:25 -0800

    got hdf5 production running on oci

--- convert_to_hdf5/README.md
+++ convert_to_hdf5/README.md
@@ -12,37 +12,36 @@ Temporary:
 2.  Run `test.sh <path to data folder>` from within this directory
 3.  Your HDF5 file will now be in in your data folder
 
-### EC2
+### OCI
 
-1.  Setup an instance of type c5ad.12xlarge
-2.  Install curl and unzip
-3.  Install aws cli (make sure to get x86 version)
-4.  install lbzip2
-5. install 
-5.  on every start, run `convert_to_hdf5/mount_instance_store.sh`
-6.  build docker file: `docker build . --tag hdf5convert`
-7.  run `./convert_to_hdf5/src/process-in-ec2.sh`
+1.  push the code files to OCI by running `./oci_utilities/push_code.sh`
+2.  Create an instance that does what you need using Oracle Linux 8 (on OCI web interface, see relevant SOP). Recomendations: AD2, Oracle Linux 8, VM.Standard2.24, private subnet, No SSH Keys, Boot Vol Size: 2TB
+3.  Enable Bastion Service on the instance
+4.  Remote into that instance. Ex:
+    `oci-cli-helpers/utilities/oci-ssh.sh $(oci-cli-helpers/utilities/ocid.sh instance flo-hdf5-1)`
+5.  Setup permissions: `OCI_CLI_AUTH=instance_principal && export OCI_CLI_AUTH`
+6.  Install the oci cli: `sudo dnf -y install oraclelinux-developer-release-el8 && sudo dnf -y install python36-oci-cli`
+7.  Pull down code onto the remote instance:
+    `oci os object bulk-download -bn 'rrl-flo-run' --download-dir "$HOME/LilFloAssessmentPipeline" --overwrite`
+8.  Run setup script: `chmod u+x "$HOME/LilFloAssessmentPipeline/oci_utilities/convert_to_hdf5/machine_setup.sh" && mkdir -p "$HOME/logs/install/" && bash "$HOME/LilFloAssessmentPipeline/oci_utilities/convert_to_hdf5/machine_setup.sh" 2>&1 | tee -a "$HOME/logs/install/$(date +"%Y-%m-%d-%H-%M-%S-%N" | cut -b1-22)"`
+9.  Run screen: `screen`. If you disconnect, reconect: `screen -r`. You could also use tmux.
+10. Run Script: ` bash "$HOME/LilFloAssessmentPipeline/oci_utilities/convert_to_hdf5/run_manual.sh" <subj number> 2>&1 | tee -a "$HOME/logs/runs/$(date +"%Y-%m-%d-%H-%M-%S-%N" | cut -b1-22)-subj_<subj number>"  `
+
+If you want to run a bunch of subjects at once, you can do that with something like:
+
+```{bash}
+for sn in 3
+do
+log="$HOME/logs/runs/$(date +"%Y-%m-%d-%H-%M-%S-%N" | cut -b1-22)-subj_$sn"
+bash "$HOME/LilFloAssessmentPipeline/oci_utilities/convert_to_hdf5/run_manual.sh" "$sn" 2>&1 | tee -a $log
+done
+```
 
 ## TODO
-1. Explore using szip as a filter for compression
-2. Explore using smaller chunk sizes
 
-## General architecture
+1.  Explore using szip as a filter for compression
+2.  Explore using smaller chunk sizes
 
-job def: subj no
-parallelization: multiple subjects at once
+### Note:
 
-1.  copy meta file for this subj
-2.  for each file in ros archive, in parallel (implemented in `convert_to_hdf5/src/run_script.bash`):
-    *   copy first bag file from s3 ros folder into local
-    *   uncompress: `lbzip2 -d filename` or other similar
-3.  for each rosbag, append the rosbag to the desired hdf5 file
-    *   use the mapping in the metadata file to determine which topics to record
-    *   scan through the hdf5 file to see if each message is within the time range for any of the segments from the meta file (note, this does not include calibration for now)
-    *   add any found messages on target topics to the hdf5 file for that experiment/activity/modality. Name hdf5 files per the segment names in the meta files ex: `in-person_simon-says_ros.hdf5`
-4.  put hdf5 file into the hdf5 s3 bucket with prefix for the appropriate subject: `flo-exp-aim1-data-hdf5/<user id, three digits: 008>`
-
-
-### Note: 
-
-Please make sure there is a extrinsics.yaml file in the /data folder 
+Please make sure there is a extrinsics.yaml file in the /data folder
---
+++ oci_utilities/convert_to_hdf5/run_manual.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+
+bucket_raw='rrl-flo-raw'
+bucket_hdf5='rrl-flo-hdf5'
+
+script="$(realpath "$0")"
+scriptpath="$(dirname "$script")"
+
+# shellcheck source=../includes/parse_input_subj_no.sh
+source "$scriptpath/../includes/parse_input_subj_no.sh"
+
+#Get the ros files of interest
+echo 'looking for mixed files (old style)'
+mixed=$(oci os object list --bucket-name "$bucket_raw" --fields name --prefix "$subject_padded" --query 'data[? ends_with("name",`"bag.bz2"`) && ! contains("name",`"podium"`) && ! contains("name",`"robot"`)] | [].name' --raw-output --all | jq '.[]' -r)
+
+echo 'looking for podium files'
+podium=$(oci os object list --bucket-name "$bucket_raw" --fields name --prefix "$subject_padded" --query 'data[? ends_with("name",`"bag.bz2"`) && contains("name",`"podium"`)] | [].name' --raw-output --all | jq '.[]' -r)
+
+echo 'looking for robot files'
+robot=$(oci os object list --bucket-name "$bucket_raw" --fields name --prefix "$subject_padded" --query 'data[? ends_with("name",`"bag.bz2"`) && contains("name",`"robot"`)] | [].name' --raw-output --all | jq '.[]' -r)
+
+[ "$(docker container ls --all --filter name=hdf5-converter -q)" ] && docker rm /hdf5-converter
+
+for group in mixed podium robot
+do
+    echo "working on $group"
+    declare -n group_files="$group"
+
+    done_first=false
+    previous_bag_fn=''
+    mkdir -p "$HOME/data/$group"
+    rm -rf "$HOME/data/"
+    mkdir -p "$HOME/data/$group"
+
+    for bag_fn in $group_files
+    do
+        bag_basename=$(basename $bag_fn)
+        #bring in the bag file from s3
+        echo "downloading $bag_basename"
+        oci os object get \
+            -bn "$bucket_raw" \
+            --file "$HOME/data/$group/$bag_basename" \
+            --name "$bag_fn"
+
+        echo "uncompressing"
+        lbzip2 -d "$HOME/data/$group/$bag_basename"
+
+        if [ $done_first = true ]
+        then
+            echo "waiting for previous file to finish"
+            hdf_exit_code=$(docker container wait hdf5-converter)
+            docker rm /hdf5-converter
+            if [ "$hdf_exit_code" -ne 0 ]
+            then
+                exit "$hdf_exit_code"
+            fi
+            rm "$HOME/data/$group/${previous_bag_fn%.*}"
+        fi
+
+        echo "starting hdf5 transfer"
+        docker run \
+            --mount type=bind,source="$HOME/data/$group/",target=/data \
+            --detach \
+            --name=hdf5-converter \
+            hdf5convert \
+            roslaunch convert_to_hdf5 convert_to_hdf5.launch bag_file:="/data/${bag_basename%.*}" out_dir:="/data"
+
+
+        previous_bag_fn="$bag_basename"
+        done_first=true
+    done
+
+    if [ $done_first = true ]
+    then
+        echo "waiting for last file to finish"
+        hdf_exit_code=$(docker container wait hdf5-converter)
+        docker rm /hdf5-converter
+        if [ "$hdf_exit_code" -ne 0 ]
+        then
+            exit "$hdf_exit_code"
+        fi
+        rm "$HOME/data/$group/${previous_bag_fn%.*}"
+    fi
+
+    echo 'done with HDF File creation, begining upload'
+
+    files2transfer=$(find "$HOME/data/$group/" -type f -printf '%P\n')
+    for fn in $files2transfer
+    do
+        oci os object put \
+            --bucket-name $bucket_hdf5 \
+            --file "$HOME/data/$group/$fn" \
+            --name "$subject_padded/$group/$fn" \
+            --force
+    done
+    echo "Done with upload"
+    rm -rf "$HOME/data/*"
+done
+
+
+rm -rf "$HOME/data/*"
+
+
+echo "--------------------------------------"
+echo "--- Done generating HDF5 files !!!!---"
+echo "--------------------------------------"
