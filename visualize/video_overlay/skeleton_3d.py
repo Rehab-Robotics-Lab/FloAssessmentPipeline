ff9164d6e0c6302dc9cd5e3848dfc6a79f3bf9e9
Michael Sobrepera <mjsobrep@live.com>
2021-12-26 16:46:16 -0800

    got 2d skeleton working

--- visualize/video_overlay/skeleton_3d.py
+++ visualize/video_overlay/skeleton_3d.py
@@ -1,39 +1,36 @@
+"""Module to export pose data as a 3d skeleton"""
 #!/usr/bin/env python3
 
 import pathlib
-import numpy as np
 import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
+import mpl_toolkits.mplot3d.axes3d as p3
 from mpl_toolkits.mplot3d import proj3d
 import h5py
-from tqdm import tqdm, trange
-import cv2
-import math
-import sys
 from common import img_overlays
 import kinematics.scripts.extract_profiles as k
-import copy
-import ipdb
 
-# Inspiration from: https://medium.com/@pnpsegonne/animating-a-3d-scatterplot-with-matplotlib-ca4b676d4b55
+# Inspiration from:
+# https://medium.com/@pnpsegonne/animating-a-3d-scatterplot-with-matplotlib-ca4b676d4b55
 
 
-def animate(iteration, data, scatters, lines, texts, ax, joint_pairs, keypoints, frames):
+def animate(iteration, data, scatters, lines, texts, axes, joint_pairs, keypoints, frames):
     for i in keypoints:
         scatters[i]._offsets3d = (
             data[iteration, i, 0:1], data[iteration, i, 1:2], data[iteration, i, 2:])
 
     for i, line in enumerate(lines):
-        line.set_data([data[iteration, joint_pairs[i][0], 0], data[iteration, joint_pairs[i][1], 0]],
-                      [data[iteration, joint_pairs[i][0], 1], data[iteration, joint_pairs[i][1], 1]])
+        line.set_data([data[iteration, joint_pairs[i][0], 0],
+                       data[iteration, joint_pairs[i][1], 0]],
+                      [data[iteration, joint_pairs[i][0], 1],
+                          data[iteration, joint_pairs[i][1], 1]])
         line.set_3d_properties(
             [data[iteration, joint_pairs[i][0], 2], data[iteration, joint_pairs[i][1], 2]])
 
     for i, text in enumerate(texts):
-        x, y, _ = proj3d.proj_transform(
-            data[iteration, i, 0], data[iteration, i, 1], data[iteration, i, 2], ax.get_proj())
-        text.set_position((x, y))
+        x_pos, y_pos, _ = proj3d.proj_transform(
+            data[iteration, i, 0], data[iteration, i, 1], data[iteration, i, 2], axes.get_proj())
+        text.set_position((x_pos, y_pos))
 
     axis_color = ['r', 'g', 'b']
     all_quivers = []
@@ -54,8 +51,10 @@ def animate(iteration, data, scatters, lines, texts, ax, joint_pairs, keypoints,
         new_quivers = []
         for i, quiver in enumerate(quivers):
             quiver.remove()
-            quiver = ax.quiver(origin[0], origin[1], origin[2], dirs[0, i], dirs[1, i], dirs[2, i],
-                               length=0.1, normalize=True, color=axis_color[i])
+            quiver = axes.quiver(
+                origin[0], origin[1], origin[2],
+                dirs[0, i], dirs[1, i], dirs[2, i],
+                length=0.1, normalize=True, color=axis_color[i])
 
             new_quivers.append(quiver)
             all_quivers.append(quiver)
@@ -69,10 +68,9 @@ def skeleton_3d(directory, cam, save=False, show=False):
     directory = pathlib.Path(directory)
     hdf5_video = h5py.File(directory/'full_data-vid.hdf5', 'r')
     hdf5_tracking = h5py.File(directory/'full_data-novid.hdf5', 'r')
-    color_dset = 'vid/color/{}/data'.format(cam)
+    cam_root = f'vid/{cam}'
 
-    ipdb.set_trace()
-    points3d = hdf5_tracking[color_dset + '-3dkeypoints-stereo']
+    points3d = hdf5_tracking[f'{cam_root}/openpose/keypoints-3d']
 
     keypoints = [4, 3, 2, 1, 5, 6, 7, 8, 0]
     joint_pairs = [(0, 1), (4, 3), (3, 2), (2, 1),
@@ -81,63 +79,68 @@ def skeleton_3d(directory, cam, save=False, show=False):
                    'lwrist', 'rshoulder', 'relbow', 'rwrists', 'waist']
 
     fig = plt.figure()
-    ax = p3.Axes3D(fig)
+    axes = p3.Axes3D(fig)
 
-    scatters = [ax.scatter(points3d[0, i, 0:1], points3d[0,
-                                                         i, 1:2], points3d[0, i, 2:]) for i in keypoints]
+    scatters = [axes.scatter(
+        points3d[0, i, 0:1], points3d[0, i, 1:2], points3d[0, i, 2:])
+        for i in keypoints]
 
-    lines = [ax.plot([points3d[0, joint[0], 0], points3d[0, joint[1], 0]],
-                     [points3d[0, joint[0], 1], points3d[0, joint[1], 1]],
-                     [points3d[0, joint[0], 2], points3d[0, joint[1], 2]],
-                     'black')[0] for joint in joint_pairs]
+    lines = [axes.plot([points3d[0, joint[0], 0], points3d[0, joint[1], 0]],
+                       [points3d[0, joint[0], 1], points3d[0, joint[1], 1]],
+                       [points3d[0, joint[0], 2], points3d[0, joint[1], 2]],
+                       'black')[0] for joint in joint_pairs]
 
-    texts = [ax.text2D(points3d[0][i, 0], points3d[0][i, 1],
-                       '%s' % (annotations[i]),
-                       size=5,
-                       zorder=1, color='k') for i in range(len(annotations))]
+    texts = [axes.text2D(points3d[0][i, 0], points3d[0][i, 1],
+                         '%s' % (annotations[i]),
+                         size=5,
+                         zorder=1, color='k') for i in range(len(annotations))]
 
     # Making frames using quivers to attack to shoulders
     frames = {}
     axis_color = ['r', 'g', 'b']
 
     rsf = k.right_shoulder_fixed_frame(points3d[0])
-    rsf_quivers = [ax.quiver(points3d[0, 2, 0], points3d[0, 2, 1], points3d[0, 2, 2],
-                             rsf[0, i], rsf[1, i], rsf[2, i], length=0.1, normalize=True, color=axis_color[i]) for i in range(3)]
+    rsf_quivers = [axes.quiver(points3d[0, 2, 0], points3d[0, 2, 1], points3d[0, 2, 2],
+                               rsf[0, i], rsf[1, i], rsf[2, i],
+                               length=0.1, normalize=True, color=axis_color[i]) for i in range(3)]
 
     rs = k.right_shoulder_moving_frame(points3d[0])
-    rs_quivers = [ax.quiver(points3d[0, 2, 0], points3d[0, 2, 1], points3d[0, 2, 2],
-                            rs[0, i], rs[1, i], rs[2, i], length=0.1, normalize=True, color=axis_color[i]) for i in range(3)]
+    rs_quivers = [axes.quiver(points3d[0, 2, 0], points3d[0, 2, 1], points3d[0, 2, 2],
+                              rs[0, i], rs[1, i], rs[2, i], length=0.1,
+                              normalize=True, color=axis_color[i]) for i in range(3)]
 
     lsf = k.left_shoulder_fixed_frame(points3d[0])
-    lsf_quivers = [ax.quiver(points3d[0, 5, 0], points3d[0, 5, 1], points3d[0, 5, 2],
-                             lsf[0, i], lsf[1, i], lsf[2, i], length=0.1, normalize=True, color=axis_color[i]) for i in range(3)]
+    lsf_quivers = [axes.quiver(points3d[0, 5, 0], points3d[0, 5, 1], points3d[0, 5, 2],
+                               lsf[0, i], lsf[1, i], lsf[2, i],
+                               length=0.1, normalize=True, color=axis_color[i]) for i in range(3)]
 
     ls = k.left_shoulder_moving_frame(points3d[0])
-    ls_quivers = [ax.quiver(points3d[0, 5, 0], points3d[0, 5, 1], points3d[0, 5, 2],
-                            ls[0, i], ls[1, i], ls[2, i], length=0.1, normalize=True, color=axis_color[i]) for i in range(3)]
+    ls_quivers = [axes.quiver(points3d[0, 5, 0], points3d[0, 5, 1], points3d[0, 5, 2],
+                              ls[0, i], ls[1, i], ls[2, i],
+                              length=0.1, normalize=True, color=axis_color[i]) for i in range(3)]
 
     frames["right_shoulder_fixed"] = rsf_quivers
     frames["right_shoulder_moving"] = rs_quivers
     frames["left_shoulder_fixed"] = lsf_quivers
     frames["left_shoulder_moving"] = ls_quivers
 
-    ax.set_xlim3d([-1, 1])
-    ax.set_xlabel('X')
+    axes.set_xlim3d([-1, 1])
+    axes.set_xlabel('X')
 
-    ax.set_ylim3d([-1, 1])
-    ax.set_ylabel('Y')
+    axes.set_ylim3d([-1, 1])
+    axes.set_ylabel('Y')
 
-    ax.set_zlim3d([0, 5])
-    ax.set_zlabel('Z')
+    axes.set_zlim3d([0, 5])
+    axes.set_zlabel('Z')
 
-    ax.set_title('3D Skeleton')
-    ax.view_init(-90, -86)
+    axes.set_title('3D Skeleton')
+    axes.view_init(-90, -86)
 
     ani = animation.FuncAnimation(fig,
                                   animate,
                                   len(points3d),
                                   fargs=(points3d, scatters, lines, texts,
-                                         ax, joint_pairs, keypoints, frames),
+                                         axes, joint_pairs, keypoints, frames),
                                   interval=50)
 
     if save:
--- visualize/video_overlay/skeleton_overlay2d.py
+++ visualize/video_overlay/skeleton_overlay2d.py
@@ -7,7 +7,9 @@ import cv2
 import math
 import sys
 import numpy as np
+from common import color
 from common import img_overlays
+from pose_body.scripts.openpose_joints import openpose_joints
 
 
 def overlay_2dSkeleton(directory, cam):
@@ -15,17 +17,17 @@ def overlay_2dSkeleton(directory, cam):
     hdf5_video = h5py.File(directory/'full_data-vid.hdf5')
     hdf5_tracking = h5py.File(directory/'full_data-novid.hdf5')
 
-    dset = 'vid/color/data/{}/data'.format(cam)
+    cam_root = f'vid/{cam}'
+    color_dset_name = f'{cam_root}/color/data'
     video_writer = cv2.VideoWriter(
-        directory/f'viz-{cam}-2dSkeleton.avi', cv2.VideoWriter_fourcc(*'MJPG'), 30, (1920, 1080))
+        str(directory/f'viz-{cam}-2dSkeleton.avi'),
+        cv2.VideoWriter_fourcc(*'MJPG'), 30, (1920, 1080))
 
-    for idx in trange(hdf5_video[dset].shape[0]):
-        img = hdf5_video[dset][idx]
-        keypoints = hdf5_tracking[dset+'-keypoints'][idx]
-        confidence = hdf5_tracking[dset+'-confidence'][idx]
-        sec = hdf5_tracking['vid/color/data/{}/secs'.format(cam)][idx]
-        nsec = hdf5_tracking['vid/color/data/{}/nsecs'.format(cam)][idx]
-        time = sec+nsec*1e-9
+    for idx in trange(hdf5_video[color_dset_name].shape[0]):
+        img = hdf5_video[color_dset_name][idx]
+        keypoints = hdf5_tracking[f'{cam_root}/openpose/keypoints'][idx]
+        confidence = hdf5_tracking[f'{cam_root}/openpose/confidence'][idx]
+        time = hdf5_tracking[f'{cam_root}/color/time'][idx]
 
         img_overlays.draw_text(img, 'frame: {}'.format(idx), pos=(100, 3))
         img_overlays.draw_text(img, 'time: {:.2f}'.format(time), pos=(500, 3))
@@ -34,23 +36,47 @@ def overlay_2dSkeleton(directory, cam):
 
         # Joints listed here: https://github.com/CMU-Perceptual-Computing-Lab/openpose/
         # blob/master/doc/02_output.md#keypoints-in-cpython
-        for joint in range(0, 9):
-            x = int(keypoints[joint][0])
-            y = int(keypoints[joint][1])
-            cv2.circle(img, (x, y), 10,
-                       img_overlays.colorScale(confidence[4], 0, 1), 8)
 
-        pairs = [(0, 1), (4, 3), (3, 2), (2, 1),
-                 (1, 5), (5, 6), (6, 7), (1, 8)]
+        joints = [openpose_joints().index(key) for key in [
+            "Nose", "LEye", "REye", "LEar", "REar", "LShoulder", "RShoulder",
+            "LElbow", "RElbow", "LWrist", "RWrist", "LHip", "RHip", "UpperNeck",
+            "HeadTop"
+        ]
+        ]
+
+        for joint in joints:
+            x_pos = int(keypoints[joint][0])
+            y_pos = int(keypoints[joint][1])
+            cv2.circle(img, (x_pos, y_pos), 10,
+                       color.color_scale(confidence[joint], 0, 1), 8)
+
+        pairs = [
+            (openpose_joints().index('UpperNeck'),
+                openpose_joints().index('LShoulder')),
+            (openpose_joints().index('UpperNeck'),
+                openpose_joints().index('RShoulder')),
+            (openpose_joints().index('LShoulder'),
+                openpose_joints().index('LElbow')),
+            (openpose_joints().index('RShoulder'),
+                openpose_joints().index('RElbow')),
+            (openpose_joints().index('LElbow'),
+                openpose_joints().index('LWrist')),
+            (openpose_joints().index('RElbow'),
+                openpose_joints().index('RWrist')),
+            (openpose_joints().index('UpperNeck'),
+                openpose_joints().index('LHip')),
+            (openpose_joints().index('UpperNeck'),
+                openpose_joints().index('RHip'))
+        ]
         for joint in pairs:
-            xs = int(keypoints[joint[0]][0])
-            ys = int(keypoints[joint[0]][1])
+            x_0 = int(keypoints[joint[0]][0])
+            y_0 = int(keypoints[joint[0]][1])
 
-            xe = int(keypoints[joint[1]][0])
-            ye = int(keypoints[joint[1]][1])
+            x_1 = int(keypoints[joint[1]][0])
+            y_1 = int(keypoints[joint[1]][1])
 
             if confidence[joint[0]] > 0.5 and confidence[joint[1]] > 0.5:
-                cv2.line(img, (xs, ys), (xe, ye), img_overlays.colorScale(
+                cv2.line(img, (x_0, y_0), (x_1, y_1), color.color_scale(
                     confidence[joint[0]] + confidence[joint[1]], 0, 1))
 
         video_writer.write(img)
