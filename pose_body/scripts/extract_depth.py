0748a4ecfcab6b6d87364d4521fecf8c6f69be73
Michael Sobrepera <mjsobrep@live.com>
2021-12-30 14:26:42 -0800

    improved tracking and 2d viz

--- pose_body/scripts/extract_depth.py
+++ pose_body/scripts/extract_depth.py
@@ -2,14 +2,18 @@
 Module to extract depth given hdf5_in and hdf5_out files
 '''
 import numpy as np
-from scipy.signal import convolve2d
 from tqdm import trange
 from common.realsense_params import MIN_VALID_DEPTH_METERS
 from common.realsense_params import MAX_VALID_DEPTH_METERS
+from common.tracking_params import DEPTH_KERNEL_SIZE
 from common.tracking_params import MAX_TIME_DISPARITY
 
+assert DEPTH_KERNEL_SIZE % 2 == 1
+MIN_VALID_DEPTH_MM = MIN_VALID_DEPTH_METERS*1000
+MAX_VALID_DEPTH_MM = MAX_VALID_DEPTH_METERS*1000
 
-def extract_depth(depth_img, keypoints, params, window_size=3):  # pylint: disable= too-many-locals
+
+def extract_depth(depth_img, keypoints, params):  # pylint: disable= too-many-locals
     '''
     Function to extract depth in the aligned camera frame given intrinsic and extrinsics
     '''
@@ -24,21 +28,29 @@ def extract_depth(depth_img, keypoints, params, window_size=3):  # pylint: disab
         # x pixel pos in depth, y pixel pos in depth, ~1 this is homogenous in camera
         k_d @ (r_cd.T @ ((inv_kc @ keypoints_with_depth.T) - t_cd))).T
     keypoints_with_depth = (inv_kc @ keypoints_with_depth.T).T
-    kernel = np.ones((window_size, window_size)) / window_size ** 2
-
-    depth_avg = convolve2d(
-        depth_img, kernel, mode='same')
 
     for i in range(keypoints_with_depth.shape[0]):
 
-        #x = int(keypoints_in_depth[i, 0] - shift)
         p_x = int(keypoints_in_depth[i, 0])
         p_y = int(keypoints_in_depth[i, 1])
 
-        if 0 <= p_y < depth_avg.shape[0] and 0 <= p_x < depth_avg.shape[1]:
-            p_z = depth_avg[p_y, p_x]
+        # if openpose doesn't know what it is doing, it will put points at x,y=0
+        if p_x == 0 or p_y == 0:
+            p_z = np.nan
         else:
-            p_z = 0
+            row, col = np.indices((DEPTH_KERNEL_SIZE, DEPTH_KERNEL_SIZE))
+            # center the indices on p_x, p_y
+            row += p_y - int(np.floor(DEPTH_KERNEL_SIZE/2))
+            col += p_x - int(np.floor(DEPTH_KERNEL_SIZE/2))
+            # create a mask of the indices that are valid
+            valid = ((row >= 0) & (row < depth_img.shape[0]) &
+                     (col >= 0) & (col < depth_img.shape[1]) &
+                     (depth_img[row, col] > MIN_VALID_DEPTH_MM) &
+                     (depth_img[row, col] < MAX_VALID_DEPTH_MM))
+            if np.any(valid):
+                p_z = np.median(depth_img[row[valid], col[valid]])
+            else:
+                p_z = np.nan
 
         keypoints_with_depth[i] = keypoints_with_depth[i] * (p_z/1000)
     return keypoints_with_depth, keypoints_in_depth[:, :2]
@@ -58,7 +70,7 @@ def add_stereo_depth(hdf5_in, hdf5_out, cam_root, transforms=None):
 
     This requires that a previous system has already discovered and saved the best
     matching depth image to the color (done by `convert_to_hdf5/src/convert_to_hdf5.py:match_depth`)
-    Any depth matches which are outside of MAX_TIME_DESCRPENCY will be saved as nan.
+    Any depth matches which are outside of MAX_TIME_DISPARITY will be saved as nan.
 
     Any depth data that falls outside of [ MIN_VALID_DEPTH_METERS, MAX_VALID_DEPTH_METERS ] will be
     saved as nan.
@@ -92,10 +104,10 @@ def add_stereo_depth(hdf5_in, hdf5_out, cam_root, transforms=None):
         keypoints3d_dset = hdf5_out[keypoints3d_dset_name]
         print('You might be running the Stereo depth extraction twice')
     if keypoints_in_depth_dset_name not in hdf5_out:
-        keypoints_in_depth = hdf5_out.create_dataset(
+        keypoints_in_depth_dset = hdf5_out.create_dataset(
             keypoints_in_depth_dset_name, (hdf5_in[color_dset_name].len(), 25, 2), dtype=np.float32)
     else:
-        keypoints_in_depth = hdf5_out[keypoints_in_depth_dset_name]
+        keypoints_in_depth_dset = hdf5_out[keypoints_in_depth_dset_name]
 
     k_c = hdf5_in[color_dset_name].attrs['K'].reshape(3, 3)
 
@@ -126,9 +138,9 @@ def add_stereo_depth(hdf5_in, hdf5_out, cam_root, transforms=None):
         if np.abs(
                 hdf5_in[depth_time_dset_name][matched_index] -
                 hdf5_in[color_time_dset_name][idx]
-        ) > MAX_TIME_DESCRPENCY:
+        ) > MAX_TIME_DISPARITY:
             keypoints3d_dset[idx, :, :] = np.NaN
-            keypoints_in_depth[idx, :, :] = np.NaN
+            keypoints_in_depth_dset[idx, :, :] = np.NaN
         else:
             depth_img = hdf5_in[depth_dset_name][matched_index]
             keypoints = hdf5_out[keypoints2d_dset_name][idx]
@@ -142,4 +154,4 @@ def add_stereo_depth(hdf5_in, hdf5_out, cam_root, transforms=None):
             kp3d[invalid_indeces, :] = np.nan
             kp_depth[invalid_indeces, :] = np.nan
             keypoints3d_dset[idx, :, :] = kp3d
-            keypoints_in_depth[idx, :, :] = kp_depth
+            keypoints_in_depth_dset[idx, :, :] = kp_depth
