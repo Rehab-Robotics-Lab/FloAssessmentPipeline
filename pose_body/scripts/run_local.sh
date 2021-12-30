45eeddf6f5afd5c6d777ebb16f29383dca5e7b3b
Michael Sobrepera <mjsobrep@live.com>
2021-12-29 18:34:21 -0800

    got pose script to have fast no-rerun and use modules

--- pose_body/scripts/run_local.sh
+++ pose_body/scripts/run_local.sh
@@ -2,11 +2,14 @@
 set -o errexit
 set -o pipefail
 
-while getopts :d:s: flag
+rerun=false
+
+while getopts :d:s:r flag
 do
     case "${flag}" in
         d) data=${OPTARG};;
         s) source=${OPTARG};;
+        r) rerun=true;;
         :) echo 'missing argument' >&2; exit 1;;
         \?) echo 'invalid option' >&2; exit 1
     esac
@@ -18,9 +21,19 @@ video_file='full_data-vid.hdf5'
 novideo_file='full_data-novid.hdf5'
 transforms_file='transforms.json'
 
+if $rerun
+then
 docker run \
     --mount type=bind,source="$data",target=/data \
     --rm \
     -it \
     openpose\
-    ./process_hdf5.py -v "/data/$video_file" -n "/data/$novideo_file" -t "/data/$transforms_file" -s "$source" -c "lower" --rerun
+    python3 -m pose_body.scripts.process_hdf5 -v "/data/$video_file" -n "/data/$novideo_file" -t "/data/$transforms_file" -s "$source" -c "lower" --rerun
+else
+docker run \
+    --mount type=bind,source="$data",target=/data \
+    --rm \
+    -it \
+    openpose\
+    python3 -m pose_body.scripts.process_hdf5 -v "/data/$video_file" -n "/data/$novideo_file" -t "/data/$transforms_file" -s "$source" -c "lower" --no-rerun
+fi
