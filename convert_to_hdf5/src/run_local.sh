94ef57bfbf11ce8ce2fc6235d86703b940258d8f
Michael Sobrepera <mjsobrep@live.com>
2022-03-01 10:32:52 -0800

    cleaned up hdf5 generation script and doc

--- convert_to_hdf5/src/run_local.sh
+++ convert_to_hdf5/src/run_local.sh
@@ -6,15 +6,21 @@ script="$(realpath "$0")"
 scriptpath="$(dirname "$script")"
 
 # parse options
-while getopts :hd: flag
+while getopts :hrd: flag
 do
     case "${flag}" in
         d) dir=${OPTARG};;
-        h) echo "pass in directory with bag files under tag -d, will output hdf5 files to the same location"; exit 0;;
+        r) rebuild=true;;
+        h) echo "pass in directory with bag files under tag -d, will output hdf5 files to the same location. Pass in with flag -r to rebuild the dockerfile used by this pipeline"; exit 0;;
         :) echo 'missing argument' >&2; exit 1;;
         \?) echo 'invalid option' >&2; exit 1
     esac
 done
+
+if [ "$rebuild" = true ] ; then
+    echo 'rebuilding docker image'
+    docker build -t hdf5convert -f "$scriptpath/../../dockerfiles/convert_to_hdf5" "$scriptpath/../../"
+fi
 
 if [ -z "$dir" ]
 then
