67a6f9528d3386bb89cbf31ffbdecce8f911a3cc
Michael Sobrepera <mjsobrep@live.com>
2022-03-06 19:26:02 -0800

    added nice plots to wrists overlay

--- common/img_overlays.py
+++ common/img_overlays.py
@@ -3,6 +3,7 @@
 Provides functions for drawing over images
 """
 import cv2
+import numpy as np
 
 
 def draw_text(img, text,  # pylint: disable=too-many-arguments
@@ -24,15 +25,16 @@ def draw_text(img, text,  # pylint: disable=too-many-arguments
         font_scale: scale to draw
         font_thickness: thickness of the lines in text
         text_color: text color as a tuple of (red, green, blue)
-        text_color_bg: background color as a tuple of (red, green, blue)
+        text_color_bg: background color as a tuple of (red, green, blue). Pass in None to suppress
         margin: the margin around the text
     """
 
     x, y = pos  # pylint: disable=invalid-name
     text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
     text_w, text_h = text_size
-    cv2.rectangle(img, (max(pos[0]-margin, 0), max(pos[1]-margin, 0)),
-                  (x + text_w+margin, y + text_h+margin), text_color_bg, -1)
+    if text_color_bg:
+        cv2.rectangle(img, (max(pos[0]-margin, 0), max(pos[1]-margin, 0)),
+                      (x + text_w+margin, y + text_h+margin), text_color_bg, -1)
     cv2.putText(img, text, (x, y + text_h + font_scale - 1),
                 font, font_scale, text_color, font_thickness)
 
@@ -60,3 +62,118 @@ def draw_cam_info(img, idx=None, time=None, cam=None, cam_type="color"):
         draw_text(img, f'time: {time:.2f}', pos=(500, 3))
     if cam:
         draw_text(img, f'view: {cam} realsense {cam_type}', pos=(900, 3))
+
+
+class DataPlot:
+    """Class to create data plots which scroll, kind of like an osciliscope
+    display.
+    """
+
+    def __init__(self, pos, size, label=None, domain_length=15,
+                 buffer=500, color=(250, 250, 250), thickness=4,
+                 border=1, blur=None):
+        """Create dataplot object
+
+        Args:
+            pos: position to place plot (x,y)
+            size: size to make plot (width, height)
+            label: Label to print for plot, None to supress
+            domain_length: the amount of the domain (amount of
+                           x-data) to plot. Plots the newest
+                           domain_length quantity of data
+            buffer: how much data to buffer. Make this >
+                    domain_length*fps
+            color: The color for the plot
+            thickness: The plot thickness
+            border: Border thickness, None to supress
+            blur: The amount of gaussian blur to add under
+                  the plot area (this is the kernel size,
+                  used to calculate st dev of the kernel).
+                  None to supress
+        """
+        #pylint: disable=too-many-arguments
+        self.x_vals = np.zeros(buffer, dtype=np.float64)
+        self.y_vals = np.zeros(buffer, dtype=np.float64)
+        self.first = True
+        self.domain_length = domain_length
+        self.pos = pos
+        self.size = size
+        # prevent division by zero if no data yet exists:
+        self.max_y = .0001
+        self.min_y = -.0001
+        self.label = label
+        self.color = color
+        self.thickness = thickness
+        self.border = border
+        self.blur = blur
+
+    def add_data(self, x_val, y_val):
+        """Add data to the plot
+
+        This is done seperately from plotting, add data as you want to,
+        plot when it makes sense
+
+        Args:
+            x_val: the x value to add
+            y_val: the y value to add
+        """
+        self.x_vals = np.roll(self.x_vals, 1)
+        self.y_vals = np.roll(self.y_vals, 1)
+        self.x_vals[0] = x_val
+        self.y_vals[0] = y_val
+        self.max_y = max(self.max_y, y_val)
+        self.min_y = min(self.min_y, y_val)
+        if self.first:
+            self.x_vals[-1] = x_val
+            self.y_vals[-1] = y_val
+            self.first = False
+            self.max_y = y_val + 0.0001
+            self.min_y = y_val - 0.0001
+
+    def plot(self, img):
+        """Plot the stored data onto an image
+
+        Args:
+            img: An image to plot onto
+        """
+        adj_x_vals = self.x_vals - self.x_vals[0]
+        in_range = adj_x_vals > (-1*self.domain_length)
+        scaled_x_vals = (adj_x_vals / self.domain_length + 1)*self.size[0]
+        # convert data to pixels
+        scaled_y_vals = self.size[1]*(
+            (self.y_vals - self.min_y)/(self.max_y - self.min_y))
+        if self.blur:
+            kernel_size = self.blur
+            left_side = max(0, self.pos[0]-kernel_size)
+            right_side = min(
+                img.shape[0], self.pos[0]+self.size[0]+kernel_size)
+            top_side = max(0, self.pos[1]-kernel_size)
+            bottom_side = min(
+                img.shape[1], self.pos[1]+self.size[1]+kernel_size)
+
+            img[self.pos[1]:(self.pos[1]+self.size[1]),
+                self.pos[0]:(self.pos[0]+self.size[0]), :] = \
+                cv2.GaussianBlur(
+                img[top_side:bottom_side, left_side:right_side, :], (kernel_size, kernel_size), 0)[
+                (self.pos[1]-top_side):(self.pos[1]+self.size[1]-bottom_side),
+                (self.pos[0]-left_side):(self.pos[0]+self.size[0]-right_side),
+                :]
+        cv2.polylines(
+            img,
+            [np.int32(
+                np.transpose(
+                    (self.pos[0] + scaled_x_vals[in_range],
+                     self.pos[1] + (scaled_y_vals[in_range]))))],
+            isClosed=False, color=self.color, thickness=self.thickness)
+        if self.border:
+            cv2.polylines(img, [np.array([
+                [self.pos[0], self.pos[1]],
+                [self.pos[0], self.pos[1] + self.size[1]],
+                [self.pos[0]+self.size[0], self.pos[1] + self.size[1]],
+                [self.pos[0]+self.size[0], self.pos[1]],
+                [self.pos[0], self.pos[1]]
+            ], np.int32)],
+                isClosed=False, color=self.color, thickness=1)
+        if self.label:
+            draw_text(img, self.label, pos=(self.pos[0]+3, self.pos[1]+3),
+                      text_color_bg=None, text_color=self.color)
--- visualize/video_overlay/overlay_wrists.py
+++ visualize/video_overlay/overlay_wrists.py
@@ -28,6 +28,14 @@ def overlay_wrists(directory, cam, dset_names):
     video_writer = cv2.VideoWriter(
         str(directory/f'viz-{cam}-wrists.avi'),
         cv2.VideoWriter_fourcc(*'MJPG'), 30, (1920, 1080))
+    left_wrist_plots = [img_overlays.DataPlot(
+        (10, 30), (400, 200), label="left wrist: x", color=(0, 200, 0), blur=25),
+        img_overlays.DataPlot(
+        (10, 240), (400, 200), label="left wrist: y", color=(200, 0, 0), blur=25)]
+    right_wrist_plots = [img_overlays.DataPlot(
+        (10, 450), (400, 200), label="right wrist: x", color=(0, 0, 200), blur=25),
+        img_overlays.DataPlot(
+        (10, 660), (400, 200), label="right wrist: y", color=(200, 200, 0), blur=25)]
     for idx in trange(hdf5_video[dset_names['color_dset']].shape[0]):
         img = hdf5_video[dset_names['color_dset']][idx]
         keypoints = hdf5_tracking[dset_names['keypoints_color']][idx]
@@ -36,12 +44,20 @@ def overlay_wrists(directory, cam, dset_names):
 
         img_overlays.draw_cam_info(img, idx, time, cam)
 
-        for joint in (openpose_joints().index("LWrist"),
-                      openpose_joints().index("RWrist")):
+        for joint, plts in zip(
+                (openpose_joints().index("LWrist"),
+                 openpose_joints().index("RWrist")),
+                (left_wrist_plots, right_wrist_plots)):
             x_pos = int(keypoints[joint][0])
             y_pos = int(keypoints[joint][1])
             cv2.circle(img, (x_pos, y_pos), 20,
                        color.color_scale(confidence[joint], 0, 1), 8)
+            # x
+            plts[0].add_data(time, x_pos)
+            plts[0].plot(img)
+            # y
+            plts[1].add_data(time, y_pos)
+            plts[1].plot(img)
 
         video_writer.write(img)
     video_writer.release()
