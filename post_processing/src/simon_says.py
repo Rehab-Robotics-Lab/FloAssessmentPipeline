d7fc09578f7a6d596725c3a4b0ad2ce31e4d65d8
Michael Sobrepera <mjsobrep@live.com>
2022-04-11 02:39:21 -0700

    more work...

--- post_processing/src/simon_says.py
+++ post_processing/src/simon_says.py
@@ -55,20 +55,22 @@ def simon_says_convex_hell(target_dir, threshold=5):
                         conv_hull = scipy.spatial.ConvexHull(
                             wrist_data-shoulder_data)
                         arm_l = arm_length.arm_length(game_rep, side)
-                        norm_ch = conv_hull.volume/(4/3*np.pi*arm_l**3)
+                        norm_ch_vol = conv_hull.volume/(4/3*np.pi*arm_l**3)
+                        norm_ch_sa = conv_hull.area/(4*np.pi*arm_l**2)
                         subj_norms = train[train["record_id"] == int(subj)]
                         bbt = subj_norms[f"bbt.z_{'right' if side=='R' else 'left'}"].values[0]
                         age = subj_norms["age"].values[0]
                         print(
                             f'subj: {subj}; {side} Arm bbt: {bbt}; age: {age}; ' +
-                            f'norm convex hull: {norm_ch}; arm length: {arm_l}')
+                            f'norm convex hull: {norm_ch_vol}; arm length: {arm_l}')
                         results.append(
-                            [subj, rep, side, bbt, age, conv_hull.volume, arm_l, norm_ch])
+                            [subj, rep, side, bbt, age, conv_hull.volume, arm_l, norm_ch_vol, conv_hull.area, norm_ch_sa])
 
     results_df = pd.DataFrame(results, columns=[
                               'subject', 'rep', 'side', 'bbt', 'age', 'convex_hull',
-                              'arm_length', 'norm_convex_hull'])
+                              'arm_length', 'norm_convex_hull', 'convex_hull_surface_area', 'convex_hull_surface_area_norm'])
     results_df.to_csv(target_dir/'ss_ch.csv')
+
 
     # target_dir = pathlib.Path("/media/mjsobrep/43CDA61E672B9161/pose/")
 if __name__ == '__main__':
--- post_processing/src/target_touch.py
+++ post_processing/src/target_touch.py
@@ -1,4 +1,4 @@
-"""Module for calculating convex hull of arm motion from target touch game"""
+"""Module for calculating kinematics of arm motion from target touch game"""
 
 import argparse
 import pathlib
@@ -13,17 +13,8 @@ from post_processing.src import arm_length
 def target_touch_accel(target_dir):
     """Calculate the convex hull for arm movement for simon says games
 
-    1. Loads data from target_dir/smoothed_data.hdf5 (generated in generate_state.py)
-    2. Determines which values to trust by taking values which are less than
-       threshold*MAD+median for variances for the wrist.
-    3. Determines arm length, taking the median of sum of segment lengths
-    4. Calculates the convex hull of the arms as the wrist positions-shoulder positions
-    5. Normalizes by dividing by maximum possible volume
-    6. Saves results to target_dir/ss_ch.csv
-
     Args:
         target_dir: The directory to process in
-        threshold: The threshold for which values to trust
     """
     #pylint: disable=too-many-locals
     target_dir = pathlib.Path(target_dir)
@@ -46,12 +37,14 @@ def target_touch_accel(target_dir):
                         data = game_rep['smooth'][joint]
                         time = game_rep['time']
 
-                        velocity = np.sqrt(
+                        speed = np.sqrt(
                             np.sum(np.power(data[:, 3:6], 2), axis=1))
                         acceleration = np.sqrt(
                             np.sum(np.power(data[:, 6:9], 2), axis=1))
-
-                        edges = np.diff(1*(velocity > 100))
+                        # has to be done on raw before taking abs
+                        jerk_raw = np.gradient(data[:, 6:9], time, axis=0)
+                        jerk = np.sqrt(np.sum(np.power(jerk_raw, 2), axis=1))
+                        edges = np.diff(1*(speed > 100))
                         start_idx = np.where(edges == 1)[0]
                         end_idx = np.where(edges == -1)[0]
                         if len(start_idx) == 0 or len(end_idx) == 0:
@@ -66,7 +59,7 @@ def target_touch_accel(target_dir):
                         for start, end in zip(start_idx, end_idx):
                             local_minima = start + \
                                 scipy.signal.argrelextrema(
-                                    velocity[start:end], np.less)[0]
+                                    speed[start:end], np.less)[0]
                             local_minima = np.insert(local_minima, 0, start)
                             local_minima = np.append(local_minima, end)
                             for idx in range(len(local_minima)-1):
@@ -76,53 +69,106 @@ def target_touch_accel(target_dir):
                         end_idx = np.array(end_idx_2)
 
                         real_movements = [
-                            np.trapz(velocity[start:end],
+                            np.trapz(speed[start:end],
                                      x=time[start:end]) > 200
                             for start, end in zip(start_idx, end_idx)]
                         start_idx = start_idx[real_movements]
                         end_idx = end_idx[real_movements]
 
+                        # note: mean acceleration is change in velocity. We already know that is small
+                        # note: mean velocity is displacement/time
+                        max_speed = []
                         max_accel = []
+                        max_jerk = []
+
+                        avg_speed = []
+                        avg_accel = []
+                        avg_jerk = []
+
+                        max_div_avg_speed = []
                         max_div_avg_accel = []
+                        max_div_avg_jerk = []
+
+                        time_to_max_speed = []
+                        time_to_max_speed_norm = []
                         time_to_max_accel = []
                         time_to_max_accel_norm = []
-                        max_vel = []
-                        time_to_max_vel = []
-                        time_to_max_vel_norm = []
-                        max_div_avg_vel = []
-                        length_movement_xyz = []
+                        time_to_max_jerk = []
+                        time_to_max_jerk_norm = []
+
+                        length_movement_xyz = []  # numerical integral of speed to get path length
                         length_movement_t = []
+
+                        normalized_jerk = []
+                        jerk_metric = []
+                        speed_metric = []  # mean speed / peek speed
+
                         num_movements = len(start_idx)
 
                         completed_segment = False
                         for start, end in zip(start_idx, end_idx):
                             seg_accel = acceleration[start:end]
-                            seg_vel = velocity[start:end]
+                            seg_speed = speed[start:end]
+                            seg_jerk = jerk[start:end]
                             seg_time = time[start:end]
                             seg_length_time = time[end]-time[start]
                             if seg_length_time < 0.1:
                                 continue
-                            area_of_accel = np.trapz(seg_accel, x=seg_time)
-                            avg_accel = area_of_accel/seg_length_time
+                            max_s = np.max(seg_speed)
                             max_a = np.max(seg_accel)
+                            max_j = np.max(seg_jerk)
+
+                            area_of_speed = np.trapz(seg_speed, x=seg_time)
+                            area_of_accel = np.trapz(seg_accel, x=seg_time)
+                            area_of_jerk = np.trapz(seg_jerk, x=seg_time)
+
+                            ttm_s = time[np.argmax(
+                                seg_speed)+start]-time[start]
+                            ttm_a = time[np.argmax(
+                                seg_accel)+start]-time[start]
+                            ttm_j = time[np.argmax(seg_jerk)+start]-time[start]
+
+                            avg_s = area_of_speed/seg_length_time
+                            avg_a = area_of_accel/seg_length_time
+                            avg_j = area_of_jerk/seg_length_time
+
+                            max_speed.append(max_s)
                             max_accel.append(max_a)
-                            max_div_avg_accel.append(max_a/avg_accel)
-                            ttma = time[np.argmax(seg_accel)+start]-time[start]
-                            time_to_max_accel.append(ttma)
-                            time_to_max_accel_norm.append(ttma/seg_length_time)
-                            max_v = np.max(seg_vel)
-                            max_vel.append(max_v)
-                            ttmv = time[np.argmax(seg_vel)+start]-time[start]
-                            time_to_max_vel.append(ttmv)
-                            time_to_max_vel_norm.append(ttmv/(seg_length_time))
-                            area_of_vel = np.trapz(seg_vel, x=seg_time)
-                            avg_vel = area_of_vel/seg_length_time
-                            max_div_avg_vel.append(max_v/avg_vel)
-                            length_movement_xyz.append(area_of_vel)
+                            max_jerk.append(max_j)
+
+                            avg_speed.append(avg_a)
+                            avg_accel.append(avg_a)
+                            avg_jerk.append(avg_j)
+
+                            max_div_avg_speed.append(max_s/avg_s)
+                            max_div_avg_accel.append(max_a/avg_a)
+                            max_div_avg_jerk.append(max_j/avg_j)
+
+                            time_to_max_speed.append(ttm_s)
+                            time_to_max_speed_norm.append(
+                                ttm_s/(seg_length_time))
+                            time_to_max_accel.append(ttm_a)
+                            time_to_max_accel_norm.append(
+                                ttm_a/seg_length_time)
+                            time_to_max_jerk.append(ttm_j)
+                            time_to_max_jerk_norm.append(ttm_j/seg_length_time)
+
+                            length_movement_xyz.append(area_of_speed)
                             length_movement_t.append(seg_length_time)
+
+                            normalized_jerk.append(
+                                np.sqrt(
+                                    (1/2) *
+                                    np.trapz(np.power(seg_jerk, 2), seg_time) *
+                                    seg_length_time *
+                                    area_of_speed
+                                ))
+
+                            jerk_metric.append(-avg_j/max_s)
+                            speed_metric.append(avg_s/max_s)
+
                             completed_segment = True
 
-                            # ttmv = time[np.argmax(velocity)]
                         if not completed_segment:
                             continue
 
@@ -143,16 +189,25 @@ def target_touch_accel(target_dir):
                                 joint,
                                 bbt,
                                 age,
+                                np.median(max_speed),
                                 np.median(max_accel),
+                                np.median(max_jerk),
+                                np.median(avg_speed),
+                                np.median(avg_accel),
+                                np.median(avg_jerk),
+                                np.median(max_div_avg_speed),
                                 np.median(max_div_avg_accel),
+                                np.median(max_div_avg_jerk),
+                                np.median(time_to_max_speed),
+                                np.median(time_to_max_speed_norm),
                                 np.median(time_to_max_accel),
                                 np.median(time_to_max_accel_norm),
-                                np.median(max_vel),
-                                np.median(time_to_max_vel),
-                                np.median(time_to_max_vel_norm),
-                                np.median(max_div_avg_vel),
+                                np.median(time_to_max_jerk),
+                                np.median(time_to_max_jerk_norm),
                                 np.median(length_movement_xyz),
                                 np.median(length_movement_t),
+                                np.median(normalized_jerk),
+                                np.median(speed_metric),
                                 num_movements])
 
     results_df = pd.DataFrame(results, columns=[
@@ -161,16 +216,25 @@ def target_touch_accel(target_dir):
                               'side',
                               'bbt',
                               'age',
-                              'max_acceleration',
-                              'max_acceleration_over_avg_acceleration',
-                              'time_to_max_acceleration',
-                              'time_to_max_acceleration_normalized',
-                              'max_velocity',
-                              'time_to_max_velocity',
-                              'time_to_max_velocity_normalized',
-                              'max_velocity_over_avg_velocity',
-                              'distance_traveled',
-                              'movement_time_length',
+                              'max_speed',
+                              'max_accel',
+                              'max_jerk',
+                              'avg_speed',
+                              'avg_accel',
+                              'avg_jerk',
+                              'max_div_avg_speed',
+                              'max_div_avg_accel',
+                              'max_div_avg_jerk',
+                              'time_to_max_speed',
+                              'time_to_max_speed_norm',
+                              'time_to_max_accel',
+                              'time_to_max_accel_norm',
+                              'time_to_max_jerk',
+                              'time_to_max_jerk_norm',
+                              'length_movement_xyz',
+                              'length_movement_t',
+                              'normalized_jerk',
+                              'speed_metric',
                               'number_movements'])
     results_df.to_csv(target_dir/'tt_features.csv')
 
